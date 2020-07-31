d231e2c751988a5f1066bbacf8f750c4a407dc99
arthur-ryoo <jene0523@gmail.com>
2020-07-30 23:44:40 -0700

    Add wind speed

--- src/App.js
+++ src/App.js
@@ -82,6 +82,7 @@ function App() {
             <div className="weather-box">
               <div className="temp">{Math.round(weather.main.temp)}°c</div>
               <div className="weather">{weather.weather[0].main}</div>
+              <div className="weather">{weather.wind.speed}</div>
             </div>
           </div>
         ) : (
